d20ad3fa2435fd0834aa0f03662ea7124c8340e8
coloph0nius <bearmaxmus@gmail.com>
2018-01-06 23:01:01 +0100

    Created routine to receive and send upd packets
    TODO: packet receiving not working?

--- LocomotiveBoard.ino
+++ LocomotiveBoard.ino
@@ -1,16 +1,25 @@
 #include <ESP8266WiFi.h>
-#include <ESP_SSD1306.h>
-#include <Adafruit_GFX.h>
 #include <SPI.h>
 #include <Wire.h>
+#include <WiFiUdp.h>
 
-const char* ssid     = "wlan";
-const char* password = "password";
+const char* ssid     = "LGB-Network";
+const char* password = "bruno2017";
+unsigned int udpPort = 4712;
+unsigned int serverPort = 4711;
+char incomingPacket[255];
+char idPacket[] = "locomotive-packet;LCE";
+char serverIP []= "192.168.10.1";
+WiFiUDP Udp;
+int _direction = 0;
+int _speed = 0;
+
 
 
 void setup() 
 {
   Serial.begin(115200);
+  Serial.println();
   Serial.println("Connecting to wifi...");
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED)
@@ -18,11 +27,49 @@ void setup()
     delay(500);
     Serial.print(".");
   }
+  Serial.println();
   Serial.println("Connected!");
+  delay (500);
+  Udp.begin(udpPort);
+  
+  Udp.beginPacket(serverIP, serverPort);
+  Udp.write(idPacket);
+  Udp.endPacket();
+  
+  
 }
+
 
 void loop() 
 {
   // put your main code here, to run repeatedly:
+  int packetSize = Udp.parsePacket();
+  if (packetSize)
+  {
+    int len = Udp.read(incomingPacket, 255);
+    if (len > 0)
+    {
+      incomingPacket[len] = 0;
+    }
+    Serial.println(incomingPacket);
+    char * results [2];
+    char * pch;
+    int i = 0;
+    pch = strtok(incomingPacket, ";");
+    while (pch != NULL)
+    {
+        results[i] = pch;
+        pch = strtok (NULL, ";");
+        i++;
+    }
+    _direction = atoi (results[0]);
+    _speed = atoi (results[1]);
+    setValues(_direction, _speed);
+  } 
+}
 
+void setValues(int direction, int speed)
+{
+  Serial.println("called..");
+  Serial.println(direction);
 }
